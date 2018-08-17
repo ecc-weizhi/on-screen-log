bd79a323cd0418ad658f3100238eb18affa96deb
Lin Weizhi <ecc.weizhi@gmail.com>
2018-08-18 00:14:23 +0800

    Added notification channel

--- library/src/main/java/app/eccweizhi/onscreenlog/NotificationController.kt
+++ library/src/main/java/app/eccweizhi/onscreenlog/NotificationController.kt
@@ -1,9 +1,12 @@
 package app.eccweizhi.onscreenlog
 
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
 import io.reactivex.subjects.BehaviorSubject
@@ -16,6 +19,7 @@ internal class NotificationController(private val context: Context,
     private val notificationManager = NotificationManagerCompat.from(context)
 
     init {
+        createNotificationChannel()
         subject.sample(SAMPLE_DURATION_MS, TimeUnit.MILLISECONDS, true)
                 .subscribe { newList: List<Message> ->
                     if (newList.isEmpty()) {
@@ -26,6 +30,20 @@ internal class NotificationController(private val context: Context,
                         notificationManager.notify(notificationId, notification)
                     }
                 }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_LOW)
+            channel.description = CHANNEL_DESCRIPTION
+
+            val notificationManager = context.getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     private fun getNotificationContentIntent(): PendingIntent {
@@ -80,7 +98,9 @@ internal class NotificationController(private val context: Context,
     }
 
     companion object {
-        private const val CHANNEL_ID = "onScreenLog"
+        private const val CHANNEL_ID = "onScreenLog"        // not user visible
+        private const val CHANNEL_NAME = "on-screen-log"    // user visible
+        private const val CHANNEL_DESCRIPTION = "Notification for on-screen-log"
         private const val CONTENT_TITLE = "Displaying logs"
         private const val NOTIFICATION_MAX_MESSAGE = 10
         private const val EMPTY_MESSAGE = "No logs yet"
